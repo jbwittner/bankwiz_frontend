a60664e51d0a67a3b2c6ffc78b419ab031e387a6
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-07-07 13:44:17 +0000

    less french words

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,43 +1,5 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import Auth0Provider from "next-auth/providers/auth0";
-
-
-export const authOptions: NextAuthOptions = {
-  secret: "secret",
-  providers: [
-    Auth0Provider({
-      clientId: "Ft19Fj18hCfT5gBF1sUjGgLzLTkoFtZz",
-      clientSecret: "Y32vrdclwYuCh8wEbDn2lOM15XDRaNfResRBcVGubTaU9MmUfHGufQHj_RTdCGU6",
-      issuer: "https://bankwiz-dev.eu.auth0.com"
-    })
-    // ...add more providers here
-  ],
-
-  callbacks: {
-    async session({ session, token, user }) {
-      // Send properties to the client, like an access_token and user id from a provider.
-      // @ts-ignore
-      session.accessToken = token.accessToken
-
-      // @ts-ignore
-      session.user.id = token.id
-      
-      return session
-
-    },
-    async jwt({ token, account, profile }) {
-      console.log("token : ", token)
-      console.log("account : ", account)
-      console.log("profile : ", profile)
-      // Persist the OAuth access_token and or the user id to the token right after signin
-      if (account) {
-        token.accessToken = account.access_token
-      }
-      return token
-    }
-  
-  },
-}
+import NextAuth from "next-auth"
+import { authOptions } from "./authoptions"
 
 const handler = NextAuth(authOptions)
 
--- app/api/token/route.ts
+++ app/api/token/route.ts
@@ -1,16 +1,19 @@
 // This is an example of how to read a JSON Web Token from an API route
 import { getServerSession } from "next-auth"
 import { getToken } from "next-auth/jwt"
-import { NextResponse } from "next/server"
-import { authOptions } from "../auth/[...nextauth]/route"
+import { NextRequest, NextResponse } from "next/server"
+import { authOptions } from "../auth/[...nextauth]/authoptions"
 
-export async function GET(req: any) {
-  //console.log("req", req)
+export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions)
   console.log("==== before session =====")
   console.log("session", session)
   // @ts-ignore
   console.log("accessToken", session?.accessToken)
   console.log("==== after session =====")
+  const token = await getToken({ req, secret: "secret" })
+  console.log("==== before token =====")
+  console.log("token", token)
+  console.log("==== after token =====")
   return NextResponse.json({ message: "Hello World" }, { status: 200 });
 }
