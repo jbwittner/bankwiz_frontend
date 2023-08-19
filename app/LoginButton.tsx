aefb9de69614c18a3c4f89561d2c7cbebf6adae8
Jean-Baptiste WITTNER <44366963+jbwittner@users.noreply.github.com>
2023-08-20 00:26:40 +0200

    add pino (#9)
    * Duh
    
    * Derp, asset redirection in dev mode
    
    * It works!

--- app/LoginButton.tsx
+++ app/LoginButton.tsx
@@ -4,9 +4,18 @@ import React from 'react';
 import { signIn, getSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import Button from '@mui/material/Button';
+import applogger from '@/tools/logger';
 
 export default function LoginButton() {
+  const child = applogger.childPageLogger('LoginButton');
+  child.info('hello world from client');
+
   const router = useRouter();
+
+  const toto = {
+    tata: 'tptp',
+    titre: 'tcoutche',
+  };
 
   const checkLogin = async () => {
     const session = await getSession();
@@ -19,10 +28,21 @@ export default function LoginButton() {
     }
   };
 
+  const checkLog = () => {
+    child.info('clivk checkLog');
+    child.warn('test warn 1 %o', toto);
+    child.warn({ toto });
+  };
+
+  child.warn('test warn 2', toto);
+
   return (
     <React.Fragment>
       <Button variant="contained" onClick={checkLogin}>
         Sign in
+      </Button>
+      <Button variant="contained" onClick={checkLog}>
+        test
       </Button>
     </React.Fragment>
   );
---
+++ tools/logger.ts
@@ -0,0 +1,32 @@
+import pino, { Logger } from 'pino';
+
+const loggerOptions: pino.LoggerOptions = {
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  browser: { asObject: true },
+  formatters: {
+    level: (label) => {
+      return { level: label.toUpperCase() };
+    },
+  },
+  timestamp: pino.stdTimeFunctions.isoTime,
+};
+
+const baseLogger: Logger = pino(loggerOptions);
+
+const applogger = {
+  base: baseLogger,
+
+  childPageLogger: (pageName: string): Logger => {
+    return baseLogger.child({ page: pageName });
+  },
+
+  childComponentLogger: (componentName: string): Logger => {
+    return baseLogger.child({ component: componentName });
+  },
+
+  childApiLogger: (endpointName: string): Logger => {
+    return baseLogger.child({ apiEndpoint: endpointName });
+  },
+};
+
+export default applogger;
