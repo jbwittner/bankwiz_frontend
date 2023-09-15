60c0aba6cc7a7970effdf2ba663bdea4e236cba3
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-09-16 00:05:20 +0200

    Merging the merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { useAuth0 } from '@auth0/auth0-react'
-import Button from '@mui/material/Button'
+import { Container, Typography, Button } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const {
-    loginWithPopup,
-    logout,
-    getAccessTokenSilently,
-    user,
-    isAuthenticated
-  } = useAuth0()
+  const { loginWithPopup, logout, getAccessTokenSilently } = useAuth0()
 
   console.log(import.meta.env.VITE_AUTH0_DOMAIN) // 123
   console.log(import.meta.env.VITE_SERVER_URL) // undefined
@@ -28,40 +18,24 @@ function App() {
   }
 
   return (
-    <>
-      <div>
-        <div>
-          {isAuthenticated && user && (
-            <div>
-              <img src={user.userpicture} alt={user.name} />
-              <h2>{user.name}</h2>
-              <p>{user.email}</p>
-            </div>
-          )}
-        </div>
-        <a href="https://vitejs.dev" rel="noreferrer" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" rel="noreferrer" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-        <Button onClick={() => loginWithPopup()}>Log In</Button>
-        <Button onClick={() => logout()}>Log out</Button>
-        <Button onClick={() => toto()}>getToken</Button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container style={{ textAlign: 'center', padding: '40px' }}>
+      <Typography variant="h3" gutterBottom>
+        Welcome to Bankwizz
+      </Typography>
+      <Typography variant="h6" gutterBottom>
+        Your Financial Management Solution
+      </Typography>
+      <Typography variant="body1" paragraph>
+        Discover how we can assist you in managing your finances in an efficient
+        and intuitive manner.
+      </Typography>
+      <Button variant="contained" color="primary" size="large">
+        Get Started
+      </Button>
+      <Button onClick={() => loginWithPopup()}>Log In</Button>
+      <Button onClick={() => logout()}>Log out</Button>
+      <Button onClick={() => toto()}>getToken</Button>
+    </Container>
   )
 }
 
