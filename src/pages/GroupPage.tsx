cc87b0d207c40bdd5477e27313e2e26f193387a0
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-09-20 23:45:33 +0200

    You know the rules and so do I

--- src/pages/GroupPage.tsx
+++ src/pages/GroupPage.tsx
@@ -41,7 +41,15 @@ export function GroupPage() {
   return (
     <React.Fragment>
       <GroupDialog open={modalIsOpen} onClose={onCloseModal} />
-      <TableContainer component={Paper} sx={{ maxWidth: '800px', marginRight: 'auto', marginLeft: 'auto', marginTop : '0' }}>
+      <TableContainer
+        component={Paper}
+        sx={{
+          maxWidth: '800px',
+          marginRight: 'auto',
+          marginLeft: 'auto',
+          marginTop: '0'
+        }}
+      >
         <Table>
           <TableHead>
             <TableRow>
