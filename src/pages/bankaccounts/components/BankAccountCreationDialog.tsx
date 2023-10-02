e38426a8e64e735dddb86c44941f1a81dbf6a22d
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-10-02 20:45:28 +0200

    Is there an award for this?

--- src/pages/bankaccounts/components/BankAccountCreationDialog.tsx
+++ src/pages/bankaccounts/components/BankAccountCreationDialog.tsx
@@ -1,7 +1,7 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material'
 import { useForm } from 'react-hook-form'
-import { useGroupCreateGroup } from '@/tools/hooks/apihooks/groupapihook'
 import { TextFieldForm } from '@/components/forms/FieldsForm'
+import { useBankAccountAddAccount } from '@/tools/hooks/apihooks/accountapihook'
 
 interface IBankAccountCreationDialogProps {
   open: boolean
@@ -10,11 +10,13 @@ interface IBankAccountCreationDialogProps {
 }
 
 interface IGroupCreationForm {
-  GroupName: string
+  AccountName: string
+  GroupId: number
+  BaseAmount: number
 }
 
 export default function BankAccountCreationDialog({ open, onCancel, onValid }: IBankAccountCreationDialogProps) {
-  const { createGroup } = useGroupCreateGroup()
+  const { addAccount } = useBankAccountAddAccount()
 
   const {
     handleSubmit,
@@ -22,12 +24,15 @@ export default function BankAccountCreationDialog({ open, onCancel, onValid }: I
     formState: { errors },
     reset
   } = useForm<IGroupCreationForm>({
-    defaultValues: { GroupName: '' }
+    defaultValues: { AccountName: '', GroupId: 0, BaseAmount: 0 }
   })
 
   const submit = async (data: IGroupCreationForm) => {
-    await createGroup({
-      groupName: data.GroupName
+    const baseAmountDecimal: number = data.BaseAmount * 100
+    await addAccount({
+      accountName: data.AccountName,
+      groupId: data.GroupId,
+      baseAmountDecimal: baseAmountDecimal
     })
     reset()
     onValid()
@@ -38,12 +43,40 @@ export default function BankAccountCreationDialog({ open, onCancel, onValid }: I
       <DialogTitle>Create new group</DialogTitle>
       <form onSubmit={handleSubmit(submit)}>
         <DialogContent>
-          <DialogContentText>Enter the informations to create a new group</DialogContentText>
-          <TextFieldForm control={control} name="GroupName" label="Group name" required error={errors.GroupName?.type === 'required'} />
+          <DialogContentText>Enter the informations to create a new bank account</DialogContentText>
+          <TextFieldForm
+            sx={{ marginTop: '6px' }}
+            control={control}
+            fullWidth={true}
+            name="AccountName"
+            label="Account name"
+            required
+            error={errors.AccountName?.type === 'required'}
+          />
+          <TextFieldForm
+            sx={{ marginTop: '6px' }}
+            control={control}
+            fullWidth={true}
+            type={'number'}
+            name="GroupId"
+            label="Group ID"
+            required
+            error={errors.GroupId?.type === 'required'}
+          />
+          <TextFieldForm
+            sx={{ marginTop: '6px' }}
+            control={control}
+            fullWidth={true}
+            type={'number'}
+            name="BaseAmount"
+            label="Base amount"
+            required
+            error={errors.BaseAmount?.type === 'required'}
+          />
         </DialogContent>
         <DialogActions>
           <Button onClick={onCancel}>Cancel</Button>
-          <Button type="submit">Create groupe</Button>
+          <Button type="submit">Create account</Button>
         </DialogActions>
       </form>
     </Dialog>
