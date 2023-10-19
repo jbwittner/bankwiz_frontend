f6aaaf948119b920db4c62d3a303bf61678646cb
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-10-19 17:13:39 +0200

    I'M PUSHING.

--- src/main.tsx
+++ src/main.tsx
@@ -1,37 +1,10 @@
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import { AppRoute, Auth0ProviderWithRedirectCallback } from './tools/router.tsx'
-import { ThemeProvider } from '@mui/material/styles'
-import { theme } from './tools/theme.ts'
-import { ToastContainer } from 'react-toastify'
-
-import 'react-toastify/dist/ReactToastify.css'
+import App from './App.tsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <ToastContainer
-      position="bottom-left"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="colored"
-    />
-    <Auth0ProviderWithRedirectCallback
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{
-        audience: import.meta.env.VITE_AUTH0_AUDIENCE,
-        redirect_uri: window.location.origin
-      }}
-    >
-      <ThemeProvider theme={theme}>
-        <AppRoute />
-      </ThemeProvider>
-    </Auth0ProviderWithRedirectCallback>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
