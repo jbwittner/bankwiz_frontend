02f0e5c39efbc8b8ab62d782f0ce276f12652fba
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-10-20 17:46:44 +0200

    Obligatory placeholder commit message

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Auth0Provider } from '@auth0/auth0-react'
 
-console.log(import.meta.env.VITE_AUTH0_DOMAIN)
-console.log(import.meta.env.VITE_AUTH0_CLIENT_ID)
-console.log(import.meta.env.VITE_AUTH0_AUDIENCE)
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <Auth0Provider
     domain={import.meta.env.VITE_AUTH0_DOMAIN}
