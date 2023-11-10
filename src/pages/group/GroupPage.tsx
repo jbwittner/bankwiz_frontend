98e6869dee88c6dbd6cf1f6ac5e8b5c0b2cfb8c6
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-11-10 18:02:57 +0100

    It's secret!

--- src/pages/group/GroupPage.tsx
+++ src/pages/group/GroupPage.tsx
@@ -1,6 +1,7 @@
 import { useGroupGetUserGroups } from '@/tools/api/server/hook/groupserviceapihook'
 import PageWrapper from '@/tools/router/pagewrapper'
 import { GroupIndexDTO } from '@jbwittner/bankwiz_openapi-client-fetch'
+import { Button, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import { useEffect, useState } from 'react'
 
 interface IGroupBasePageProps {
@@ -11,9 +12,31 @@ const GroupPagePage = (props: IGroupBasePageProps) => {
   return (
     <div>
       <h1>HomePage</h1>
-      {props.groupIndexDTO.map(groupIndexDTO => {
-        return <div key={groupIndexDTO.groupId}>{groupIndexDTO.groupId + ' - ' + groupIndexDTO.groupName}</div>
-      })}
+      <TableContainer component={Paper} sx={{ maxWidth: 650 }}>
+        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Group ID</TableCell>
+              <TableCell align="center">Group Name</TableCell>
+              <TableCell align="center">Action</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {props.groupIndexDTO.map(groupIndexDTO => {
+              return (
+                <TableRow key={groupIndexDTO.groupId} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                  <TableCell component="th" scope="row">
+                    {groupIndexDTO.groupId}
+                  </TableCell>
+                  <TableCell align="center">{groupIndexDTO.groupName}</TableCell>
+                  <TableCell align="center"><Button variant="text">Text</Button>
+</TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   )
 }
