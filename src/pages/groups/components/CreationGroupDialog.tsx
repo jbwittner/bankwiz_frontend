1879892d7756344fcb3b05e0c89fbd08fa9d97d3
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2023-11-11 23:45:57 +0100

    Don't tell me you're too blind to see

--- src/pages/groups/components/CreationGroupDialog.tsx
+++ src/pages/groups/components/CreationGroupDialog.tsx
@@ -13,7 +13,6 @@ interface IFormGroupCreation {
   groupName: string
 }
 
-
 export const CreationGroupDialog = (props: ICreationGroupDialogProps) => {
   const { createGroup } = useGroupServiceApi()
 
