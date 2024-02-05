bac392b7d1121d6b74179c144d8a055444de02c5
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-02-05 17:23:46 +0100

    WTF is this.

---
+++ openapi/src/apis/index.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './BankAccountServiceApi';
+export * from './GroupServiceApi';
+export * from './TransactionServiceApi';
+export * from './UserServiceApi';
