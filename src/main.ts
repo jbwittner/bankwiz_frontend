d4a690a55881d9376cfc979ed97e4bb5880effc7
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-10-31 23:44:35 +0100

    lets drink beer

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createAuth0 } from '@auth0/auth0-vue';
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(
+  createAuth0({
+    domain: "bankwiz-development.eu.auth0.com",
+    clientId: "SXP4oUqfpIIdLBqsJ6SFxIqyuZqhgReU",
+    authorizationParams: {
+      audience: "bankwiz_server",
+      redirect_uri: window.location.origin
+    }
+  })
+);
 
 app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,14 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
+    <LoginButton />
     <TheWelcome />
   </main>
 </template>
+
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+import LoginButton from '@/components/LoginButton.vue'
+console.log("HomeView.vue")
+
+
+</script>
