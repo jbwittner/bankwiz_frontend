3fa9829383c6ee276d6e15391c84ffbc2395808c
Jean-Baptiste WITTNER <44366963+jbwittner@users.noreply.github.com>
2024-11-09 15:12:23 +0100

    Add auth0 (#1)
    * lets drink beer
    
    * TODO: Fix later
    
    * Please forgive me
    
    * lol

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,22 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { createAuth0 } from '@auth0/auth0-vue'
+
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(
+  createAuth0({
+    domain: import.meta.env.VITE_DOMAIN_AUTH0,
+    clientId: import.meta.env.VITE_CLIENT_ID_AUTH0,
+    authorizationParams: {
+      audience: import.meta.env.VITE_AUDIENCE_AUTH0,
+      redirect_uri: window.location.origin,
+    },
+  }),
+)
 
 app.mount('#app')
