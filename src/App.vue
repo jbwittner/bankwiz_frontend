e88f6c2321e4ec9400fd7cf265933e1fecf433cd
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-11-11 21:39:56 +0100

    happy monday _ bleh _

--- src/App.vue
+++ src/App.vue
@@ -5,11 +5,15 @@ import BasicToolBar from '@/components/BasicToolBar.vue'
 </script>
 
 <template>
-  <header class="header">
-
+  <div class="header">
+aaaz
+  <!--
   <BasicToolBar />
+  -->
 
-  </header>
+  </div>
+
+  <!--
 
   <div class="main">
   <RouterView />
@@ -17,19 +21,36 @@ import BasicToolBar from '@/components/BasicToolBar.vue'
   <RouterView />
   <RouterView />
   </div>
+  -->
+
 </template>
 
-<style scoped>
+<style>
+#app {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  background-color: gray;
+  padding: 0 !important;
+  margin: 0 !important;
+}
+
 .header {
   width: 100%;          /* Prend toute la largeur */
   height: 50px;         /* Hauteur fixe de 50px */
-  position: fixed;      /* Reste fixe en haut de la page */
-  top: 0;               /* Aligné tout en haut */
-  left: 0;              /* Commence depuis le côté gauche */
-  display: flex;
-  align-items: center;   /* Centrage vertical du contenu */
-  justify-content: center; /* Centrage horizontal du contenu */
-  z-index: 1000;        /* Pour qu'il passe au-dessus des autres éléments */
+  //position: fixed;      /* Reste fixe en haut de la page */
+  //top: 0;               /* Aligné tout en haut */
+  //left: 0;              /* Commence depuis le côté gauche */
+  //display: flex;
+  //align-items: center;   /* Centrage vertical du contenu */
+  //justify-content: center; /* Centrage horizontal du contenu */
+  //z-index: 1000;        /* Pour qu'il passe au-dessus des autres éléments */
+  background-color: #0d47a1;
+  border-bottom-color: red;
+  border-bottom: 10px;
+  padding:  0;
 }
 .main {
   padding-top: 50px; /* Compense la hauteur du header fixe */
