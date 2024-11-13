46b4d5f15cc30839ab3292558df19aff083675cc
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-11-13 18:46:41 +0100

    I am Spartacus

--- eslint.config.js
+++ eslint.config.js
@@ -12,17 +12,17 @@ export default [
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**', 'cypress/support/**'],
   },
 
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  
+
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
-  
+
   {
     ...pluginCypress.configs.recommended,
     files: [
