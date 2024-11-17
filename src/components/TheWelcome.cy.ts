7d1be9a828b2a9f7473fa907a0c09b61e0acf9ab
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-11-17 22:12:33 +0100

    That last commit was cringe

--- src/components/TheWelcome.cy.ts
+++
@@ -1,8 +0,0 @@
-import HelloWorld from "./HelloWorld.vue"
-
-describe('TheWelcome.cy.ts', () => {
-  it('playground', () => {
-    cy.mount(HelloWorld)
-    cy.get('h3').contains("You’ve successfully")
-  })
-})
