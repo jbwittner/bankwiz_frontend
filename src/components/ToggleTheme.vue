643c1026e637439715f9bc6c800089b17b67fb7a
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-11-18 10:01:43 +0100

    Added translation.

---
+++ src/components/ToggleTheme.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-app>
+    <v-btn @click="toggleTheme">toggle theme</v-btn>
+  </v-app>
+</template>
+
+<script setup>
+import { useTheme } from 'vuetify'
+
+const theme = useTheme()
+
+function toggleTheme () {
+  theme.global.name.value = theme.global.current.value.dark ? 'light' : 'dark'
+}
+</script>
