9069b65c982ea056c45dc5e9cf532a1e7437155a
Jean-Baptiste WITTNER <jb.wittner@outlook.com>
2024-11-23 15:32:55 +0100

    I'm totally adding this to epic win. +300

--- cypress/components/TheWelcome.cy.ts
+++
@@ -1,8 +0,0 @@
-import HelloWorld from "../../src/components/HelloWorld.vue";
-
-describe('TheWelcome.cy.ts', () => {
-  it('playground', () => {
-    cy.mount(HelloWorld)
-    cy.get('h3').contains("You’ve successfully")
-  })
-})
